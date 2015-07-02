8307bb081960768684bad5dd0ed2cbb4cefe4d8d
nadavys <nadavys@yahoo.com>
2015-07-01 17:20:25 -0700

    use given logo

--- app/scripts/controllers/error.js
+++ app/scripts/controllers/error.js
@@ -4,11 +4,11 @@ angular.module('stormpathIdpApp')
   .controller('ErrorCtrl', function ($scope,Stormpath) {
     $scope.errors = Stormpath.errors;
     $scope.inError = false;
-    todo: uncomment!!!!
+    // todo: uncomment!!!!
     $scope.$watchCollection('errors',function(){
       $scope.inError = $scope.errors.length > 0;
     });
 
   //note: this is hardcoded, might need to remove this
-  $scope.logoUrl = "/images/logo.png"
+  // $scope.logoUrl = "/images/logo.png"
   });
